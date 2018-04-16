46a4b411cbc6a238ac5df09289c94c374604b272
Peter Banfield <peter.e.banfield@gmail.com>
2018-04-16 15:34:28 -0500

    modularized so that can be used by other files

--- cleanData.py
+++ cleanData.py
@@ -1,6 +1,24 @@
 import os
 
-filePath = os.path.join('Datasets', 'Cleaned', 'CGL2017.txt')
+def addToDict(clss, inDict, attend):
+    """Input: clss = fields of interest, inDict = existing dictionary, attend = integer attendace for game
+    Output: dictionary containing the new information"""
+    # Case 1: new class found
+    if clss not in inDict:
+        inDict[clss] = (attend, 1)
+    
+    # Case 2: class in dict
+    else:
+        inDict[clss] = (inDict[clss][0] + attend, inDict[clss][1] + 1)
+    
+    return inDict
+
+def avgAttend(inDict):
+    """Input: dict where key = class, values = (sumAttendance, numSum)
+    Output: dict where key = class, values = avg attendance"""
+    for k in inDict:
+        inDict[k] = inDict[k][0] / inDict[k][1]
+    return inDict
 
 def normalize(classDict): 
     """Input: classDict has dictionary with keys = class, values = average attend
@@ -20,51 +38,81 @@ def normalize(classDict):
 
     return classList
 
-# key = class, element = (sumAttend, numSum)
-dayTimeDict = {}
-visitDict = {}
-parkDict = {}
-
-with open(filePath, 'r') as inFile:
-
-    for line in inFile:
-        elements = line.split(',')
-
-        # Case 1: new day+time class found
-        if (elements[1], elements[6]) not in dayTimeDict:
-            dayTimeDict[(elements[1], elements[6])] = (int(elements[8]), 1)
+def normPark(parkID, filePath=os.path.join('Datasets', 'Cleaned', 'CGL2017.txt')):
+    """Input: parkId representing the stadium, path to data file
+    Output: list of normalized class lists with first element in each list being name of list"""
+    dayTimeDict = {}
+    visitDict = {}
+    parkDict = {}
+    
+    with open(filePath, 'r') as data:
         
-        # Case 2: day+night class in dict
-        else:
-            dayTimeDict[(elements[1], elements[6])] = (dayTimeDict[(elements[1], elements[6])][0]+int(elements[8]), dayTimeDict[(elements[1], elements[6])][1]+1)
+        for line in data:
+            elements = line.strip().split(',')
+            if elements[7] != parkID:
+                continue
+            
+            dayTimeDict = addToDict((elements[1], elements[6]), dayTimeDict, int(elements[8]))
+            visitDict = addToDict(elements[2], visitDict, int(elements[8]))
+            parkDict = addToDict(elements[7], parkDict, int(elements[8]))
 
-        # Case 1: new visit class found
-        if elements[2] not in visitDict:
-            visitDict[elements[2]] = (int(elements[8]), 1)
-        
-        # Case 2: visit class in dict
-        else:
-            visitDict[elements[2]] = (visitDict[elements[2]][0] + int(elements[8]), visitDict[elements[2]][1] + 1)
-        
-        # Case 1: new park class found
-        if elements[7] not in parkDict:
-            parkDict[elements[7]] = (int(elements[8]), 1)
-        
-        # Case 2: park class in dict
-        else:
-            parkDict[elements[7]] = (parkDict[elements[7]][0] + int(elements[8]), parkDict[elements[7]][1] + 1)
+    dayTimeDict = avgAttend(dayTimeDict)
+    visitDict = avgAttend(visitDict)
+    parkDict = avgAttend(parkDict)
 
-dayTimeList = normalize(dayTimeDict)
-visitList = normalize(visitDict)
-parkList = normalize(parkDict)
+    dayTimeList = normalize(dayTimeDict)
+    visitList = normalize(visitDict)
+    parkList = normalize(parkDict) 
 
-outFilePath = os.path.join('normalizedClasses.txt')
-listNorm = [dayTimeList, visitList, parkList]
+    dayTimeList.insert(0, 'dayTimeList')
+    visitList.insert(0, 'visitList')
+    parkList.insert(0, 'parkList')
+
+    masterList = [dayTimeList, visitList, parkList]    
+    
+    return masterList
+
+parkID = 'HOU03'
+filePath = os.path.join('Datasets', 'Cleaned', 'CGL2017.txt')
+
+
+
+# # key = class, element = (sumAttend, numSum)
+# dayTimeDict = {}
+# visitDict = {}
+# parkDict = {}
+
+# with open(filePath, 'r') as inFile:
+
+#     for line in inFile:
+#         elements = line.split(',')
+
+#         dayTimeDict = addToDict((elements[1], elements[6]), dayTimeDict, int(elements[8]))
+#         visitDict = addToDict(elements[2], visitDict, int(elements[8]))
+#         parkDict = addToDict(elements[7], parkDict, int(elements[8]))        
+
+# dayTimeDict = avgAttend(dayTimeDict)
+# visitDict = avgAttend(visitDict)
+# parkDict = avgAttend(parkDict)
+
+# dayTimeList = normalize(dayTimeDict)
+# visitList = normalize(visitDict)
+# parkList = normalize(parkDict)
+
+outFilePath = os.path.join(parkID+'Classes.txt')
+
+listNorm = normPark(parkID, filePath)
 
 with open(outFilePath, 'w') as outFile:
     for l in listNorm:
         for clss in l:
-            if clss == l[-1]:
+            if type(clss) == type((1,2)):
+                clss = clss[0]+clss[1]
+                if clss == l[-1][0]+l[-1][1]:
+                    outFile.write(str(clss)+'\n')
+                else:
+                    outFile.write(str(clss)+',')
+            elif clss == l[-1]:
                 outFile.write(str(clss)+'\n')
             else:
                 outFile.write(str(clss)+',')
