6e948b7380c52087522b33b7b11ee6824ce58766
gollygeemr <38505571+gollygeemr@users.noreply.github.com>
2018-04-18 12:29:31 -0500

    added win%

--- Datasets/cleanFile.py
+++ Datasets/cleanFile.py
@@ -2,26 +2,42 @@ import os
 
 inFilePath = os.path.join('Datasets', 'gl2010_17', 'GL2017.TXT')
 outFilePath = os.path.join('Datasets', 'Cleaned', 'CGL2017.txt')
-
 with open(inFilePath, 'r') as rf:
     with open(outFilePath, 'w') as of:
 
-        elmList = [0, 2, 3, 5, 6, 8, 12, 16, 17, 160]
+        elmList = [0, 2, 3, 5, 6, 8, 12, 16, 17, 160, 1, 13, 14, 15, 9, 10]
 
+        teamDict = {}
         for line in rf:
-            try:
-                if line.split(',')[17] == '':
-                    continue
-                if int(line.split(',')[17]) == 0: 
-                    continue
-            except:
-                print(line)
-                break
-
+            l = line.strip().split(",")            
             for e in elmList:
-                if e == elmList[-1]:
-                    of.write(line.split(',')[e].strip().replace('"', ''))
+                    
+                if e == elmList[-1] and e != 10:
+                    of.write(l[e].replace('"', ''))
                 else:
-                    of.write(line.split(',')[e].replace('"', '')+',')
+                    of.write(l[e].replace('"', '')+',')
+                if e == 10:#find win percent
+                    if l[6] not in teamDict:
+                        teamDict[l[6]] = [0,0]
+                    if l[3] not in teamDict:
+                        teamDict[l[3]] = [0,0]
+                    if int(l[10]) > int(l[9]):
+                        teamDict[l[6]][0] += 1
+                    elif int(l[10]) < int(l[9]):
+                        teamDict[l[3]][0] += 1
+                    else:
+                        teamDict[l[6]][0] += .5
+                        teamDict[l[3]][0] += .5
+                    teamDict[l[6]][1] += 1
+                    teamDict[l[3]][1] += 1
+
+                    hteamwp = round(teamDict[l[6]][0]/teamDict[l[6]][1], 3)
+                    vteamwp = round(teamDict[l[3]][0]/teamDict[l[3]][1], 3)
+                    print(vteamwp, hteamwp)
+                    
+                    of.write(str(vteamwp)+","+str(hteamwp))
+                    if e != elmList[-1]:
+                        of.write(",")
 
             of.write('\n')
+
