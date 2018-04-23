5d318f69f6b98d6415e2173230e2783b75871b42
Peter Banfield <peter.e.banfield@gmail.com>
2018-04-23 15:31:02 -0500

    added 2d subplot and 3d ploting

--- graphData.py
+++ graphData.py
@@ -3,7 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+def subPlot2D(X, Y, weights):
+    fig, (sp1, sp2) = plt.subplots(1,2)
+    
+    sp1x = [i[1] for i in X]
+    sp1.plot(sp1x, Y, 'bo')
+    xmin = min(sp1x) 
+    xmax = max(sp1x)
+    x = [j for j in range(int(xmin),int(xmax)+1)]
+    y = [weights[0][0]+weights[1][0] * e for e in x]
+    sp1.plot(x,y)
+    sp1.set_title('Attendence vs Day+Time')
 
+    sp2x = [i[2] for i in X]
+    sp2.plot(sp2x, Y, 'bo')
+    xmin = min(sp2x) 
+    xmax = max(sp2x)
+    x = [j for j in range(int(xmin),int(xmax)+1)]
+    y = [weights[0][0]+weights[2][0] * e for e in x]
+    sp2.plot(x,y)
+    sp2.set_title('Attendence vs Away Team')
+
+    
+    plt.show()
 
 def twoDPlot(x, y, weights):
     plt.plot(x,y, 'bo')
@@ -16,18 +38,34 @@ def twoDPlot(x, y, weights):
     plt.plot(X, Y)
     plt.show()
 
-def threeDPlot(x, y, z, weights):
-    xmin = min(x) 
-    xmax = max(x)
-    ymin = min(y) 
-    ymax = max(y)
+def threeDPlot(x, z, weights):
+    
+    gx = [i[1] for i in x]
+    gy = [i[2] for i in x]
+    xmin = min(gx) 
+    xmax = max(gx)
+    ymin = min(gy) 
+    ymax = max(gy)
+
+
+    x = np.arange(xmin, xmax, 1)
+    y = np.arange(ymin, ymax, 1)
+
+    X, Y = np.meshgrid(x, y)
+
+    Z = weights[0][0] + weights[1][0] * X + weights[2][0] * Y
+
+   
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    Axes3D.scatter(ax,x,y)
-    X = np.linspace(xmin,xmax).tolist()
-    Y = np.linspace(ymin,ymax).tolist()
-    Z = []
-    for i in range(len(X)):
-        Z.append(weights[0][0] + weights[1][0] * X[i] + weights[2][0] * Y[i])
+    az = np.array(z)
+    agx = np.array(gx)
+    agy = np.array(gy)
+    ax.scatter(agx,agy,az, c='r')
+    ax.plot_wireframe(X,Y,Z)
 
-    Axes3D.plot_surface(X,Y,Z)
+    ax.set_xlabel('Day+Time')
+    ax.set_ylabel('Away Team')
+    ax.set_zlabel('Attendance')
+
+    plt.show()
