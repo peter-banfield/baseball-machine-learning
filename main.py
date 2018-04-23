ee6cae62da6781f0a61c5895f179e0a49737be43
Peter Banfield <peter.e.banfield@gmail.com>
2018-04-23 12:02:13 -0500

    minor changes

--- main.py
+++ main.py
@@ -28,13 +28,13 @@ with open(dataPath, 'r') as data:
             # # time+day
             # x.append([1,classDict['dayTimeList'].index((l[1],l[6]))])
             # visit
-            # x.append([1,classDict['visitList'].index(l[2])])
-            x.append([1,classDict['dayTimeList'].index((l[1],l[6])),classDict['visitList'].index(l[2])])
+            x.append([1,classDict['visitList'].index(l[2])])
+            # x.append([1,classDict['dayTimeList'].index((l[1],l[6])),classDict['visitList'].index(l[2])])
 
 w = regress(x, y)
 gx = [i[1] for i in x]
-gy = [i[2] for i in x]
+# gy = [i[2] for i in x]
 # print(w)
-# twoDPlot(gx,y,w)
-threeDPlot(gx,gy,y,w)
+twoDPlot(gx,y,w)
+# threeDPlot(gx,gy,y,w)
 
