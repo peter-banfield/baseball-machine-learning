061f85abe4be5683cf93aa367ae3936ad81acf22
Peter Banfield <peter.e.banfield@gmail.com>
2018-04-23 12:00:16 -0500

    Revert "Changed normPark to normData"
    This reverts commit dea6134f792a13288720c9eeb865b700fc6e8219.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import numpy as np 
 import matplotlib.pyplot as plt
-from cleanData import normData
+from cleanData import normPark
 from linearRegression import regress
 from graphData import twoDPlot, threeDPlot
 
 parkID = 'HOU03'
 dataPath = os.path.join('Datasets', 'Cleaned', 'CGL2017.txt')
-MasterList = normData(parkID)
+MasterList = normPark(parkID)
 
 # masterList = [dayTimeList, visitList]    
 classDict = {}
